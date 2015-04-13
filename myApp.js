140f46a07e326a4f1135e768fb092afb6372fc2e
Edwin Lunandy <populationtwo@gmail.com>
2015-04-13 18:31:15 -0400

    V2 milestone.

--- myApp.js
+++ myApp.js
@@ -2,6 +2,18 @@
  * Created by P2 on 4/13/2015.
  */
 angular.module('myApp',[])
-	.controller('MultiplicationCtrl', function($scope){
-		$scope.numbers = [1,2,3,4,5,6,7,8,9,10];
+	.controller('MultiplicationCtrl', function($scope, $attrs){
+		$scope.numberLimit = $attrs.initialNumberLimit || 10;
+
+		$scope.numbers = function(){
+			var numbers = [];
+			for (var i = 0; i<$scope.numberLimit; i++){
+				numbers[i] = i + 1;
+			};
+			return numbers;
+		}
+
+		$scope.compute = function(a,b){
+			return a * b;
+		}
 	})
